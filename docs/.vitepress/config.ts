33bb117d9b10681dee6c8b2efb13bb690eb93e3e
rainsoso <rainsosole@gmail.com>
2025-12-31 16:23:22 +0800

    -

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -31,6 +31,6 @@ export default defineConfig({
     // socialLinks: [
     //   { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     // ]
-    base: '/KnowBase/',
-  }
+  },
+  base: '/KnowBase/'
 })
